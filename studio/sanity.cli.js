01c4e99123f04591a161595300a6011b3e7d8d03
tztz-beep <tzharel123@gmail.com>
2026-06-21 19:01:03 +0000

    Add Sanity Studio project

---
+++ studio/sanity.cli.js
@@ -0,0 +1,15 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'nk1s624p',
+    dataset: 'production'
+  },
+  deployment: {
+    /**
+     * Enable auto-updates for studios.
+     * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
+     */
+    autoUpdates: true,
+  }
+})
